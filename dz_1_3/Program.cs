0b8df1326f89a47fe00b457659ab3299d4325fb7
Максим <fedyush90@mail.ru>
2022-09-04 15:30:36 +0300

    четное не четное число

---
+++ dz_1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
